ab6a505f67174eb42c6227ada7c8892e517ff288
木村大希 <kimurataiki@kimurataikinoMacBook-Air.local>
2024-01-20 23:39:34 +0900

    created env file

--- application.py
+++ application.py
@@ -3,49 +3,63 @@ import pandas as pd
 from pandasai import SmartDataframe as smartdf
 from pandasai.llm.openai import OpenAI
 import matplotlib
+import os
+import glob
 matplotlib.use('Agg')
 
 
 # your api key
-api_key = "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" # 木村で発行した私用のopenai APIキー
-OPENAI_API_KEY = api_key
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 # OpenAIのllmインスタンス
-llm = OpenAI(api_token=OPENAI_API_KEY)
+llm = OpenAI(api_token=OPENAI_API_KEY, model="gpt-4-1106-preview")
 # PandasAIインスタンス
-df = pd.DataFrame({"拠点":["拠点1","拠点2","拠点3"], "人数":[3,4,5], "電気":["停電有り", "停電無し", "調査中"]})
+df = pd.read_csv('static/sample_data.csv')
 dfHtml = df.to_html(classes='my-table')
 full_path = 'static/'
 
-config = {"save_charts":True, "save_charts_path":full_path, "llm": llm, "enable_cache": False}
+config = {
+    "save_charts":True,
+    "save_charts_path":full_path,
+    "llm": llm,
+    "enable_cache": False,
+    "verbose":True
+    }
 
 pandas_ai = smartdf(df, config=config)
-
+resultElements = []
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def main():
     print("request method:", request.method)
-
     if request.method == 'GET':
         return render_template("page.html",dataTable = dfHtml, result="<div></div>")
     elif request.method == 'POST':
         query = request.form["query"]
         answer = pandas_ai.chat(query+"画像の場合はtemp_chart.pngとして保存して下さい。")
 
-        if type(answer) is str or type(answer) is int:
-            div = f'<div>{str(answer)}</div>'
-        elif type(answer) is smartdf:
-            div = f'<div>{str(answer)}</div>'
-        else:
-            print("answer image:", answer)
-            div = "<img src = f'{full_path}temp_chart.png'></div>"
 
-        return render_template("page.html",dataTable = dfHtml, result=div)
+        if type(answer) is str and 'png' in answer:
+            print("answer image:", answer)
+            # listPngs = glob.glob('static/*.png')
+            # latest_file = max(listPngs, key=os.path.getctime)
+            # div = f"<img src = '{latest_file}'>"
+            div = f"<img src = '{answer}'>"
+        
+        elif type(answer) is str or type(answer) is int:
+            div = f'<div>Q:{query}:{str(answer)}</div>'
+
+        elif type(answer) is smartdf:
+            dfHtmlResult = answer.to_html(classes='my-table')
+            div = dfHtmlResult
+        
+
+        resultElements.append(div)
+        print("resultList:",resultElements)
+        return render_template("page.html",dataTable = dfHtml, results=resultElements)
 
 ## 実行
 if __name__ == "__main__":
-    try:
-        app.run(debug=True)
-    except Exception as e:
-        print(e)
+    app.run(debug=True)
+    
